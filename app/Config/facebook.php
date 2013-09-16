d6475a2995bb32de434e78e7737d825f53f172eb
cele <celeweidmann@gmail.com>
2013-09-16 18:52:33 -0300

    Configuración de la aplicación de facebook.

---
+++ app/Config/facebook.php
@@ -0,0 +1,32 @@
+<?php
+/**
+  * Get an api_key and secret from facebook and fill in this content.
+  * save the file to app/Config/facebook.php
+  */
+/* 
+ * Config para radarcultural.com.ar
+ */
+	$config = array(
+  		'Facebook' => array(
+  			'appId' => '206237702878239',
+  			'apiKey' => '206237702878239',
+  			'secret' => '76396f8dd9fb59e8384e0114a462c7f1',
+  			'cookie' => true,
+  			'locale' => 'en_US',
+  		)
+  	);
+ 
+ //Config para radar.workspace.com
+ /*
+ 	$config = array(
+  		'Facebook' => array(
+  			'appId' => '486390234789438',
+  			'apiKey' => '486390234789438',
+  			'secret' => '634f7ea49c5338c05c6f4e1629b34dbf',
+  			'cookie' => true,
+  			'locale' => 'en_US',
+  		)
+  	);
+  * 
+  */
+?>
