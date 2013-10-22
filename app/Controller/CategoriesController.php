ba43294ed5e332e8519938ede07851db686508da
Ale Prieto <aleprieto@gmail.com>
2013-10-22 12:32:41 -0300

    Se limitan las ediciones de Categorias sólo para el usuario admin

--- app/Controller/CategoriesController.php
+++ app/Controller/CategoriesController.php
@@ -6,78 +6,61 @@ App::uses('AppController', 'Controller');
  * @property Category $Category
  */
 class CategoriesController extends AppController {
-	
+
 	public $components = array('RequestHandler');
 
 	/**************************************************************************************************************
 	 *  Authentication
-	**************************************************************************************************************/
-    public function beforeFilter() {
-        parent::beforeFilter();
-        $this -> Auth -> allow('index');
-    }
-	
-	public function isAuthorized($user = null) {
-	    # All registered users can add events
-	    if ($this->action === 'index') {
-	        return true;
-	    }
-	
-	    // # The owner of a category can edit and delete it
-	    // if (in_array($this->action, array('edit', 'delete'))) {
-	        // $categoryId = $this->request->params['pass'][0];
-	        // if ($this->Event->isOwnedBy($categoryId, $user['id'])) {
-	            // return true;
-	        // }
-	    // }
-	
-	    return parent::isAuthorized($user);
+	 **************************************************************************************************************/
+	public function beforeFilter() {
+		parent::beforeFilter();
+		$this->Auth->allow('index');
 	}
+
+	public function isAuthorized($user = null) {
+		// # All registered users can add events
+		// if ($this->action === 'index') {
+		// return true;
+		// }
+
+		# Admin Users can do admin_actions
+		$admin_actions = array(
+			'admin_add',
+			'admin_delete',
+			'admin_edit',
+			'admin_index'
+		);
+		if (in_array($this->action, $admin_actions)) {
+			if (isset($user['role']) && $user['role'] === 'admin') {
+				return true;
+			}
+		}
+
+		// # The owner of a category can edit and delete it
+		// if (in_array($this->action, array('edit', 'delete'))) {
+		// $categoryId = $this->request->params['pass'][0];
+		// if ($this->Event->isOwnedBy($categoryId, $user['id'])) {
+		// return true;
+		// }
+		// }
+
+		return parent::isAuthorized($user);
+	}
+
 	/**************************************************************************************************************
 	 *  /authentication
-	**************************************************************************************************************/
-		
+	 **************************************************************************************************************/
 
-/**
- * index method
- *
- * @return void
- */
-	public function index() {
-		// debug($this->request->isAjax());
-	    if ($this->request->isAjax()) {
-	        $this->Category->recursive = -1;
-			$categories = $this->Category->find('all');
-			$this->set(array('categories' => $categories, '_serialize' => array('categories')));
-			return;
-            // return json_encode($this->Category->find('all'));
-        }
-        throw new NotFoundException("Categoría Inválida", 404);       
-		// $this->Category->recursive = 0;
-		// $this->set('categories', $this->paginate());
-	}
+	/**************************************************************************************************************
+	 *  Admin Users
+	 **************************************************************************************************************/
 
-/**
- * view method
- *
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
-	public function view($id = null) {
-		if (!$this->Category->exists($id)) {
-			throw new NotFoundException(__('Invalid category'));
-		}
-		$options = array('conditions' => array('Category.' . $this->Category->primaryKey => $id));
-		$this->set('category', $this->Category->find('first', $options));
-	}
-
-/**
- * add method
- *
- * @return void
- */
-	public function add() {
+	/**
+	 * admin_add method
+	 *
+	 * @return void
+	 */
+	public function admin_add() {
 		if ($this->request->is('post')) {
 			$this->Category->create();
 			if ($this->Category->save($this->request->data)) {
@@ -89,14 +72,14 @@ class CategoriesController extends AppController {
 		}
 	}
 
-/**
- * edit method
- *
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
-	public function edit($id = null) {
+	/**
+	 * admin_edit method
+	 *
+	 * @throws NotFoundException
+	 * @param string $id
+	 * @return void
+	 */
+	public function admin_edit($id = null) {
 		if (!$this->Category->exists($id)) {
 			throw new NotFoundException(__('Invalid category'));
 		}
@@ -113,15 +96,15 @@ class CategoriesController extends AppController {
 		}
 	}
 
-/**
- * delete method
- *
- * @throws NotFoundException
- * @throws MethodNotAllowedException
- * @param string $id
- * @return void
- */
-	public function delete($id = null) {
+	/**
+	 * admin_delete method
+	 *
+	 * @throws NotFoundException
+	 * @throws MethodNotAllowedException
+	 * @param string $id
+	 * @return void
+	 */
+	public function admin_delete($id = null) {
 		$this->Category->id = $id;
 		if (!$this->Category->exists()) {
 			throw new NotFoundException(__('Invalid category'));
@@ -134,4 +117,69 @@ class CategoriesController extends AppController {
 		$this->Session->setFlash(__('Category was not deleted'));
 		$this->redirect(array('action' => 'index'));
 	}
+
+	/**
+	 * admin_index method
+	 *
+	 * @return void
+	 */
+	public function admin_index() {
+		// debug($this->request->isAjax());
+		// if ($this->request->isAjax()) {
+		// $this->Category->recursive = -1;
+		// $categories = $this->Category->find('all');
+		// $this->set(array(
+		// 'categories' => $categories,
+		// '_serialize' => array('categories')
+		// ));
+		// return;
+		// // return json_encode($this->Category->find('all'));
+		// }
+		// throw new NotFoundException("Categoría Inválida", 404);
+
+		$this->Category->recursive = 0;
+		$this->set('categories', $this->paginate());
+	}
+
+	/**************************************************************************************************************
+	 *  /admin users
+	 **************************************************************************************************************/
+
+	/**
+	 * index method
+	 *
+	 * @return void
+	 */
+	public function index() {
+		// debug($this->request->isAjax());
+		if ($this->request->isAjax()) {
+			$this->Category->recursive = -1;
+			$categories = $this->Category->find('all');
+			$this->set(array(
+				'categories' => $categories,
+				'_serialize' => array('categories')
+			));
+			return;
+			// return json_encode($this->Category->find('all'));
+		}
+		throw new NotFoundException("Categoría Inválida", 404);
+		// $this->Category->recursive = 0;
+		// $this->set('categories', $this->paginate());
+	}
+
+	/**
+	 * view method
+	 *
+	 * @throws NotFoundException
+	 * @param string $id
+	 * @return void
+	 */
+	public function view($id = null) {
+		if (!$this->Category->exists($id)) {
+			throw new NotFoundException(__('Invalid category'));
+		}
+		$options = array('conditions' => array('Category.' . $this->Category->primaryKey => $id));
+		$this->set('category', $this->Category->find('first', $options));
+	}
+
 }
