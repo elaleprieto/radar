5adf6ab5dd9448ad409bb0e1d6623200173d76d7
Ale Prieto <aleprieto@gmail.com>
2013-10-28 14:21:26 -0300

    Android

--- app/Controller/EventsController.php
+++ app/Controller/EventsController.php
@@ -14,7 +14,7 @@
 		**************************************************************************************************************/
         public function beforeFilter() {
             parent::beforeFilter();
-            $this -> Auth -> allow('get', 'index', 'view');
+            $this -> Auth -> allow('get', 'getJSON', 'index', 'view');
         }
 		
 		public function isAuthorized($user = null) {
@@ -253,6 +253,101 @@
                     // return $this->set(array('events' => $events, '_serialize' => array('events')));
                     $this->set(array('events' => $events, '_serialize' => array('events')));
 					return;
+                }
+            }
+        }
+
+		public function getJSON() {
+            $this -> autoRender = FALSE;
+
+            if ($this -> request -> is('get')) {
+                $eventCategory = isset($this -> request -> query['eventCategory']) ? $this -> request -> query['eventCategory'] : array();
+                $eventInterval = $this -> request -> query['eventInterval'];
+                $neLat = $this -> request -> query['neLat'];
+                $neLong = $this -> request -> query['neLong'];
+                $swLat = $this -> request -> query['swLat'];
+                $swLong = $this -> request -> query['swLong'];
+                if (isset($neLat) && isset($neLong) && isset($swLat) && isset($swLong)) {
+                    switch ($eventInterval) {
+                        case '2' :
+                            $tomorrow = strtotime("+1 days");
+                            $intervalConditions = array('OR' => array(
+                                    array('AND' => array(
+                                            array('Event.date_start >=' => date("Y-m-d", $tomorrow)),
+                                            # # Tomorrow
+                                            array('Event.date_start <' => date("Y-m-d", strtotime("+$eventInterval days")))
+                                            # # After Tomorrow
+                                        )),
+                                    array('AND' => array(
+                                            'Event.date_start <' => date("Y-m-d", $tomorrow),
+                                            'Event.date_end >=' => date("Y-m-d", $tomorrow)
+                                        )),
+                                ));
+                            break;
+
+                        default :
+                            $intervalConditions = array('OR' => array(
+                                    array('AND' => array(
+                                            array('Event.date_start >=' => date("Y-m-d")),
+                                            array('Event.date_start <' => date("Y-m-d", strtotime("+$eventInterval days")))
+                                        )),
+                                    array('AND' => array(
+                                            'Event.date_start <' => date("Y-m-d"),
+                                            'Event.date_end >=' => date("Y-m-d")
+                                        )),
+                                ));
+                            break;
+                    }
+                    $options['conditions'] = array(
+                        'Event.lat <' => $neLat,
+                        'Event.lat >' => $swLat,
+                        'Event.long <' => $neLong,
+                        'Event.long >' => $swLong,
+                        $intervalConditions
+                    );
+                    $categories = array();
+                    $eventCategory = json_decode($eventCategory); # Angular lo manda en formato JSON
+                    if (sizeof($eventCategory) > 0) {
+                        $categoryConditions = array();
+                        foreach ($eventCategory as $key => $category) {
+                            array_push($categoryConditions, array('CategoriesEvent.category_id =' => $category));
+                        }
+                        // array_push($conditions, array("OR" => $categoryConditions));
+                        array_push($options['conditions'], array("OR" => $categoryConditions));
+                    }
+                    $options['fields'] = array('Event.id'
+                        , 'Event.title'
+                        , 'Event.address'
+                        , 'Event.date_start'
+                        , 'Event.date_end'
+                        , 'Event.lat'
+                        , 'Event.long'
+                        , 'CategoriesEvent.category_id'
+                        , 'Category.icon'
+                    );
+					$options['group'] = array('Event.id');
+					$options['recursive'] = 0;
+					
+					$options['joins'] = array(
+					    array('table' => 'categories_events',
+					        'alias' => 'CategoriesEvent',
+					        'type' => 'LEFT',
+					        'conditions' => array(
+					            'Event.id = CategoriesEvent.event_id',
+					        )
+					    )
+					    , array('table' => 'categories',
+					        'alias' => 'Category',
+					        'type' => 'LEFT',
+					        'conditions' => array(
+					            'Category.id = CategoriesEvent.category_id',
+					        )
+					    )
+					);
+
+                    // $this -> Event -> bindModel(array('hasOne' => array('CategoriesEvents')));
+                    $events = $this -> Event -> find('all', $options);
+                    return json_encode($events);
                 }
             }
         }
