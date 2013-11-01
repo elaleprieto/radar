43d4acc5114a8d13b13a89b4952c262c9b803c92
cele <celeweidmann@gmail.com>
2013-11-01 13:04:55 -0300

    Model/Category. Validación que el nombre de la categoría sea único. Que el icon no esté vacío.

--- app/Model/Category.php
+++ app/Model/Category.php
@@ -22,6 +22,19 @@ class Category extends AppModel {
 				//'last' => false, // Stop validation after this rule
 				//'on' => 'create', // Limit validation to 'create' or 'update' operations
 			),
+			'unique' => array(
+        		'rule' => 'isUnique',
+        	),
+		),
+		'icon' => array(
+			'notempty' => array(
+				'rule' => array('notempty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
 		),
 		'sort' => array(
 			'numeric' => array(
